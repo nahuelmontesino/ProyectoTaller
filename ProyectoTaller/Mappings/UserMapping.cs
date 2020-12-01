9c3104a512a6a0cdb3fc8076f674c43f9259d5ba
Nahuel <montesinonahuel@gmail.com>
2020-12-01 16:38:52 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoTaller/Mappings/UserMapping.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Mappings
+{
+    public class UserMapping: ClassMap<User>
+    {
+        public UserMapping()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name).Not.Nullable();
+            Map(x => x.LastName).Not.Nullable();
+            Table("User");
+        }
+    }
+}
