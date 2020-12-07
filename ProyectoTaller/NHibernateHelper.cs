535efd4d987f370ffc5dc48a407871fc2266ec7c
Nahuel <montesinonahuel@gmail.com>
2020-12-07 12:03:04 -0300

    mappings with automapper configuration

--- ProyectoTaller/NHibernateHelper.cs
+++ ProyectoTaller/NHibernateHelper.cs
@@ -3,18 +3,7 @@ using FluentNHibernate.Automapping;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using NHibernate;
-using NHibernate.Cfg;
-using NHibernate.Connection;
-using NHibernate.Dialect;
-using NHibernate.Driver;
-using NHibernate.Mapping.ByCode;
 using NHibernate.Tool.hbm2ddl;
-using ProyectoTaller;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace API
 {
@@ -35,13 +24,14 @@ namespace API
         {
             //var connectionString = System.Configuration.ConfigurationManager.AppSettings["ConnectionString"];
             var connectionString = "Server=localhost;Database=Proyecto_Taller;Trusted_Connection=True;";
-
+            var cfg = new NHibernetConfiguration();
             _sessionFactory = Fluently.Configure()
 
                .Database(MsSqlConfiguration.MsSql2012
                .ConnectionString(connectionString).ShowSql())
                .Mappings(m =>
-                            m.FluentMappings.AddFromAssemblyOf<User>())
+                     m.AutoMappings.Add(
+                     AutoMap.AssemblyOf<User>(cfg)))
                .ExposeConfiguration(cfg => new SchemaExport(cfg)
                .Create(false, false))
 
