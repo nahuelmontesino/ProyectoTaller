cc3b41e8998f2a507cc741724bd83a2ef5d58f70
Nahuel <montesinonahuel@gmail.com>
2021-01-19 16:43:14 -0300

    feat: interfaces to implement repositories

---
+++ ProyectoTaller/Interfaces/IQuestionRepository.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    interface IQuestionRepository : IRepository<Question>
+    {
+        void SaveQuestions(ISource pSource, string pDificulty, string pCategory, int pAmount, ISession session);
+        void AddOption(Option pOption);
+        void DeleteAllQuestions();
+        List<Question> GetQuestions(int pSet, int pDificulty, int pCategory, int pAmount);
+        List<string> GetCategoriesOfSet(ISource pSource, int set);
+        List<string> GetDifficultiesOfCategory(ISource pSource, int set, int category);
+    }
+}
---
+++ ProyectoTaller/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity pEntity);
+        void Remove(TEntity pEntity);
+        TEntity GetByID(int pId);
+        IEnumerable<TEntity> GetAll();
+    }
+}
