71b3bd1efa2e2aeeb8a0fdc3f70b8b3030b8114f
Nahuel <montesinonahuel@gmail.com>
2021-01-19 16:43:56 -0300

    feat: interface to implement many datasources

---
+++ ProyectoTaller/Interfaces/ISource.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface ISource
+    {
+        string Url { get; }
+        string Name { get; }
+        Dictionary<int, string> CategoryDictionary { get; }
+        Dictionary<int, string> DifficultyDictionary { get; }
+        List<Question> GetQuestions(string pDificulty, int pCategory, int pAmount);
+        int GetDifficultyFactor(int difficulty);
+        int GetTimeFactor(double time);
+    }
+}
